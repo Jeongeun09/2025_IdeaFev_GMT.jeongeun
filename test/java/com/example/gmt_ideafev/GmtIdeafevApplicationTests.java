4dca0e2d2a2b644b7714106b08209b708098dc42
CHUN JEONGEUN <s25054@gsm.hs.kr>
2025-08-30 23:48:47 +0900

    git 이상

---
+++ test/java/com/example/gmt_ideafev/GmtIdeafevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gmt_ideafev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmtIdeafevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
